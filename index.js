698e9ec215ff88d9887441cbf5d02f0723969520
Kate Frolova <47864430+aqua6858@users.noreply.github.com>
2021-07-15 16:32:19 +0300

    вернул точки с запятой

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const axios = require("axios");
-const onlinesim_token = "8bc6532baa5ee0647d10333fe5a0841d"
+const axios = require("axios");;
+const onlinesim_token = "8bc6532baa5ee0647d10333fe5a0841d";
 
 
-function getSMS(test) 
+function getSMS(test)
 {
-    return test
+    return test;
 }
 
+async function start(){
+await getSMS("1");
+}
 
-await getSMS("1")
-
+start();
