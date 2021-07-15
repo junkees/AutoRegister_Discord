9ba6e7e8347ff8f7ec78e7466dd5c5373b46fe72
Junkes <danya@onlyawp.ru>
2021-07-15 15:49:50 +0300

    new files

---
+++ index.js
@@ -0,0 +1,9 @@
+const request = require('request')
+const onlinesim_token = "8bc6532baa5ee0647d10333fe5a0841d"
+
+async function getSMS(test) {
+    return test
+}
+
+
+await console.log(getSMS("123"))
