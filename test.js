847a13ee367c8a8500c15174f68206fd20e65dcf
Danya <kefirawp@gmail.com>
2023-05-29 13:33:50 +0300

    Delete test.js

--- test.js
+++
@@ -1,7 +0,0 @@
-const axios = require('axios')
-
-axios.get("https://onlyawp.ru/axiooos", {
-    headers: {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:90.0) Gecko/20100101 Firefox/90.0"
-    }
-})
